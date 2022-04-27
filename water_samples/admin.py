288a585ff0429db79b63857ef161d7a80d595e8c
caesar699 <caesar699@gmail.com>
2022-04-27 15:34:25 +0600

    setup model

--- water_samples/admin.py
+++ water_samples/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
-# Register your models here.
+from .models import School
+
+
+class SchoolAdmin(LeafletGeoAdmin):
+    list_display = ['name', 'province', 'district', 'level', 'male', 'female']
+
+
+admin.site.register(School, SchoolAdmin)
